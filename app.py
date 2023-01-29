c32bc51f12c4d67edfa1e84644c2c83d2a7f375b
Joe Fearnley <joe.fearnley@gmail.com>
2023-01-29 15:15:03 -0500

    getting form validation working

--- app.py
+++ app.py
@@ -1,10 +1,50 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from flask_wtf.csrf import CSRFProtect
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+csrf = CSRFProtect()
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-if __name__ == '__main__':
-    app.run(debug=True)
+csrf.init_app(app)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.post('/process-file')
+def process_file():
+    if 'file' not in request.files:
+        flash('Please select a file.')
+        return redirect(url_for('index'))
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('Please select a file.')
+        return redirect(url_for('index'))
+
+    if file:
+        if not allowed_file(file.filename):
+            flash('Invalid file type. Please choose one of the accepted below.')
+            return redirect(url_for('index'))
+
+        filename = secure_filename(file.filename)
+
+        # proces flle...
+        print('processing file.....')
+
+        return redirect(url_for('download_file', name=filename))
+
+    return redirect(url_for('index'))
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
