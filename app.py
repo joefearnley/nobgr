bafa97c8e16657c5d0476fbb5dd88b2205ff806f
Joe Fearnley <joe.fearnley@gmail.com>
2023-02-04 16:12:39 -0500

    Attempting to write rinsed file[

--- app.py
+++ app.py
@@ -42,22 +42,26 @@ def index_post():
             return redirect(request.url)
 
         filename = secure_filename(file.filename)
-        filename = str(time.time()) + '_' + filename
+        filename = str(time.time()).replace(".", "") + '_' + filename
 
         # save the file
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/rinsed/', filename))
 
-        # input_path = filename 
-        # output_path = 
+        input_path = filename
+        output_filename = 'rinsed_' + filename
+        output_path = os.path.join(app.config['UPLOAD_FOLDER'] + 'rinsed/', output_filename)
 
-        # with open(input_path, 'rb') as i:
-        #     with open(output_path, 'wb') as o:
-        #         input = i.read()
-        #         output = remove(input)
-        #         o.write(output)
+        print(output_path)
 
-        return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
+        with open(input_path, 'rb') as i:
+            with open(output_path, 'wb') as o:
+                input = i.read()
+                output = remove(input)
+                o.write(output)
 
+                return send_from_directory(app.config["UPLOAD_FOLDER"], output_path)
+
+    flash('No file provided.')
     return redirect(request.url)
 
 # if __name__ == '__main__':
